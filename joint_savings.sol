7f585d8991716ad21863d0df998992e2df697b61
Darcy Davis <133836349+darcy5d@users.noreply.github.com>
2023-11-18 18:10:42 +1100

    Update joint_savings.sol 2023-11-18 18:12

--- joint_savings.sol
+++ joint_savings.sol
@@ -83,7 +83,8 @@ contract JointSavings {
         /*
         Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
         */
-        // YOUR CODE HERE!
+        
+        contractBalance = address(this).balance;
     }
 
     /*
@@ -92,11 +93,15 @@ contract JointSavings {
     function setAccounts(address payable account1, address payable account2) public{
 
         // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
-        // YOUR CODE HERE!
+        
+        accountOne = account1;
+        accountTwo = account2;
     }
 
     /*
     Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
     */
-    // YOUR CODE HERE!
+    function() external payable {
+
+    }
 }
